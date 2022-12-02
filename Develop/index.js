9701f22f8f91bfad82d2ca6951d97d6c83010f52
devin <devinelliotgomez@gmail.com>
2022-12-01 18:16:51 -0600

    finished all input type questions in the index, started working on the list questions as well as added a new folder to put the generated markdown file in

--- Develop/index.js
+++ Develop/index.js
@@ -8,8 +8,8 @@ const questions = [
         type: 'input',
         name: 'title',
         message: 'What would you like the title of your project to be?',
-        validate: userInput => {
-            if (userInput) {
+        validate: userTitleInput => {
+            if (userTitleInput) {
                 return true;
             } else {
                 console.log('you need to enter a title.')
@@ -17,6 +17,103 @@ const questions = [
             }
         }
 
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your github username.',
+        validate: gitInput => {
+            if (gitInput) {
+                return true;
+            } else {
+                console.log('you need to enter your github username.')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('please enter your email.')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe what your application is for',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('you need to enter a description.')
+                return false;
+            }
+        }
+
+
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'how will a developer use this application?',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('you need to enter a value to countinue.')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How does one use your application and what is required for it to work?',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('you need to enter a value.')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributors',
+        message: 'are there any contributors with your project?',
+        validate: contributorsInput => {
+            if (contributorsInput) {
+                return true;
+            } else {
+                console.log('if there are none, simply put n/a.')
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How does one test the application?',
+        validate: userTitleInput => {
+            if (userTitleInput) {
+                return true;
+            } else {
+                console.log('please enter in how to test the application.')
+                return false;
+            }
+        }   
+    },
+    {
+        type: 'list',
+        name: 'license'
     }
 ];
 
